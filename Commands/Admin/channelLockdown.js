b6f0fea0910979ba03f71d74ca2aad36184522a7
Sashking <sashking.cz@gmail.com>
2021-09-03 18:51:41 +0200

    Added lockdown commands

---
+++ Commands/Admin/channelLockdown.js
@@ -0,0 +1,26 @@
+const { CommandInteraction, Client, MessageEmbed, Message } = require('discord.js');
+
+module.exports = {
+    name: 'lockdown',
+    description: "Закрыть канал.",
+    userPermissions: ['MANAGE_CHANNELS'],
+
+    /**
+     * @param {Client} client
+     * @param {CommandInteraction} interaction
+     * @param {String[]} args
+     */
+    run: async(client, interaction, args) => {
+
+        await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+            VIEW_CHANNEL: false,
+        });
+
+        const successEmbed = new MessageEmbed()
+            .setDescription(`Канал ${interaction.channel} успешно закрыт.`)
+            .setColor(client.color(interaction.guild))
+
+        await interaction.followUp({ embeds: [ successEmbed ] });
+
+    }
+}
